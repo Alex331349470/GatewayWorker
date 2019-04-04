bc2141694c6b0006c1c247c67f45bd0acff4248f
Alex331349470 <331349470@qq.com>
2019-04-04 15:53:29 +0800

    fix the bug

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -31,35 +31,35 @@ class Events
     /**
      * 当客户端连接时触发
      * 如果业务不需此回调可以删除onConnect
-     * 
+     *
      * @param int $client_id 连接id
      */
-    public static function onConnect($client_id)
-    {
-        // 向当前client_id发送数据 
-        Gateway::sendToClient($client_id, "Hello $client_id\r\n");
-        // 向所有人发送
-        Gateway::sendToAll("$client_id login\r\n");
-    }
-    
+    // public static function onConnect($client_id)
+    // {
+    //     // 向当前client_id发送数据
+    //     Gateway::sendToClient($client_id, "Hello $client_id\r\n");
+    //     // 向所有人发送
+    //     // Gateway::sendToAll("$client_id login\r\n");
+    // }
+
    /**
     * 当客户端发来消息时触发
     * @param int $client_id 连接id
     * @param mixed $message 具体消息
     */
-   public static function onMessage($client_id, $message)
-   {
-        // 向所有人发送 
-        Gateway::sendToAll("$client_id said $message\r\n");
-   }
-   
+   // public static function onMessage($client_id, $message)
+   // {
+   //      // 向所有人发送
+   //      // Gateway::sendToAll("$client_id said $message\r\n");
+   // }
+
    /**
     * 当用户断开连接时触发
     * @param int $client_id 连接id
     */
-   public static function onClose($client_id)
-   {
-       // 向所有人发送 
-       GateWay::sendToAll("$client_id logout\r\n");
-   }
+   // public static function onClose($client_id)
+   // {
+   //     // 向所有人发送
+   //     // GateWay::sendToAll("$client_id logout\r\n");
+   // }
 }
