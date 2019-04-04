f4de3c23af5f0867ee4c992b5a4e9ebfe96df8dc
Alex331349470 <331349470@qq.com>
2019-04-04 15:55:03 +0800

    fix the bug

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -42,21 +42,21 @@ class Events
     //     // Gateway::sendToAll("$client_id login\r\n");
     // }
 
-   /**
-    * 当客户端发来消息时触发
-    * @param int $client_id 连接id
-    * @param mixed $message 具体消息
-    */
-   // public static function onMessage($client_id, $message)
-   // {
-   //      // 向所有人发送
-   //      // Gateway::sendToAll("$client_id said $message\r\n");
-   // }
+    /**
+     * 当客户端发来消息时触发
+     * @param int $client_id 连接id
+     * @param mixed $message 具体消息
+     */
+    public static function onMessage($client_id, $message)
+    {
+        // 向所有人发送
+        // Gateway::sendToAll("$client_id said $message\r\n");
+    }
 
-   /**
-    * 当用户断开连接时触发
-    * @param int $client_id 连接id
-    */
+    /**
+     * 当用户断开连接时触发
+     * @param int $client_id 连接id
+     */
    // public static function onClose($client_id)
    // {
    //     // 向所有人发送
